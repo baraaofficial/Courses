b585f1d6bc677f6c01172ebde7c26ea650984fc1
baraaofficial <baraa_official@outlook.com>
2020-12-24 18:37:49 +0200

    update for Charts

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -21,24 +21,24 @@ class HomeController extends Controller
         // End chart users new
 
         // Start chart users trashed
-        $Charts_today_users_trashed = User::onlyTrashed()->whereDate('created_at', today())->count();
-        $Charts_yesterday_users_trashed = User::onlyTrashed()->whereDate('created_at', today()->subDays(2))->count();
-        $Charts_users_trashed_3_days_ago = User::onlyTrashed()->whereDate('created_at', today()->subDays(3))->count();
-        $Charts_users_trashed_4_days_ago = User::onlyTrashed()->whereDate('created_at', today()->subDays(4))->count();
-        $Charts_users_trashed_5_days_ago = User::onlyTrashed()->whereDate('created_at', today()->subDays(5))->count();
-        $Charts_users_trashed_6_days_ago = User::onlyTrashed()->whereDate('created_at', today()->subDays(6))->count();
-        $Charts_users_trashed_7_days_ago = User::onlyTrashed()->whereDate('created_at', today()->subDays(7))->count();
-        // End chart users block
+        $Charts_today_users_trashed = User::onlyTrashed()->whereDate('deleted_at', today())->count();
+        $Charts_yesterday_users_trashed = User::onlyTrashed()->whereDate('deleted_at', today()->subDays(2))->count();
+        $Charts_users_trashed_3_days_ago = User::onlyTrashed()->whereDate('deleted_at', today()->subDays(3))->count();
+        $Charts_users_trashed_4_days_ago = User::onlyTrashed()->whereDate('deleted_at', today()->subDays(4))->count();
+        $Charts_users_trashed_5_days_ago = User::onlyTrashed()->whereDate('deleted_at', today()->subDays(5))->count();
+        $Charts_users_trashed_6_days_ago = User::onlyTrashed()->whereDate('deleted_at', today()->subDays(6))->count();
+        $Charts_users_trashed_7_days_ago = User::onlyTrashed()->whereDate('deleted_at', today()->subDays(7))->count();
+        // End chart users trashed
 
 
         // Start chart users block
-        $Charts_today_users_block = User::status()->whereDate('created_at', today())->count();
-        $Charts_yesterday_users_block = User::status()->whereDate('created_at', today()->subDays(2))->count();
-        $Charts_users_block_3_days_ago = User::status()->whereDate('created_at', today()->subDays(3))->count();
-        $Charts_users_block_4_days_ago = User::status()->whereDate('created_at', today()->subDays(4))->count();
-        $Charts_users_block_5_days_ago = User::status()->whereDate('created_at', today()->subDays(5))->count();
-        $Charts_users_block_6_days_ago = User::status()->whereDate('created_at', today()->subDays(6))->count();
-        $Charts_users_block_7_days_ago = User::status()->whereDate('created_at', today()->subDays(7))->count();
+        $Charts_today_users_block = User::status()->whereDate('updated_at', today())->count();
+        $Charts_yesterday_users_block = User::status()->whereDate('updated_at', today()->subDays(2))->count();
+        $Charts_users_block_3_days_ago = User::status()->whereDate('updated_at', today()->subDays(3))->count();
+        $Charts_users_block_4_days_ago = User::status()->whereDate('updated_at', today()->subDays(4))->count();
+        $Charts_users_block_5_days_ago = User::status()->whereDate('updated_at', today()->subDays(5))->count();
+        $Charts_users_block_6_days_ago = User::status()->whereDate('updated_at', today()->subDays(6))->count();
+        $Charts_users_block_7_days_ago = User::status()->whereDate('updated_at', today()->subDays(7))->count();
         // End chart users block
 
         return view('admin.home',
@@ -49,10 +49,10 @@ class HomeController extends Controller
                 'Charts_today_users_block','Charts_yesterday_users_block','Charts_users_block_3_days_ago',
                 'Charts_users_block_4_days_ago','Charts_users_block_5_days_ago','Charts_users_block_6_days_ago','Charts_users_block_7_days_ago',
                 //end var charts users block
-                //start var charts users block
+                //start var charts users trashed
                 'Charts_today_users_trashed','Charts_yesterday_users_trashed','Charts_users_trashed_3_days_ago',
                 'Charts_users_trashed_4_days_ago','Charts_users_trashed_5_days_ago','Charts_users_trashed_6_days_ago','Charts_users_trashed_7_days_ago'
-                //start var charts users block
+                //end var charts users trashed
             ));
     }
 }
